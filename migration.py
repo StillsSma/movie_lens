7c41ba68166d24713a51be4065724658cabd4ab2
StillsSma <samdawson301@live.com>
2016-10-05 13:55:18 -0400

    final commit of movie_lens assignment

--- migration.py
+++ migration.py
@@ -6,33 +6,6 @@ import csv
 connection = psycopg2.connect("dbname=movie_lens user=movie_lens")
 cursor = connection.cursor()
 
-with open('u.data', encoding = "latin1") as f:
-    reader = csv.reader(f, delimiter='\t')
-    item = []
-    for row in reader:
-        item.append(row)
-
-cursor.execute("DROP TABLE IF EXISTS main_data;")
-
-create_table_command = """
-CREATE TABLE main_data (
-    userid int,
-    itemid int,
-    rating int,
-    timestmp int
-
-);
-"""
-
-cursor.execute(create_table_command)
-for row in item:
-    cursor.execute("INSERT INTO main_data VALUES (%s,%s,%s,%s)",
-    (row[0],row[1],row[2],row[3]))
-
-
-
-
-
 
 with open('u.item', encoding = "latin1") as g:
     reader2 = csv.reader(g, delimiter='|')
@@ -44,11 +17,11 @@ cursor.execute("DROP TABLE IF EXISTS movie_data;")
 
 create_table_command = """
 CREATE TABLE movie_data (
-    movie_id int,
-    movie_title VARCHAR(50),
-    release_dte date,
+    movie_id int Primary Key,
+    movie_title VARCHAR(100),
+    release_dte VARCHAR(11),
     videorelease_dte VARCHAR(10),
-    IMDbURL VARCHAR(50),
+    IMDbURL VARCHAR(150),
     unknown boolean,
     Action boolean,
     Adventure boolean,
@@ -81,24 +54,58 @@ for row in item2:
 
 
 
-with open('u.genre', encoding = "latin1") as h:
+with open('u.user', encoding = "latin1") as h:
     reader3 = csv.reader(h, delimiter='|')
 
+    item3 = []
+    for row in reader3:
+        item3.append(row)
 
-cursor.execute("DROP TABLE IF EXISTS genre;")
+
+cursor.execute("DROP TABLE IF EXISTS userinfo;")
 
 create_table_command = """
-CREATE TABLE genre (
-    id int,
-    name varchar(20)
+CREATE TABLE userinfo (
+    userid int Primary Key ,
+    age int,
+    gender Varchar(1),
+    occupation Varchar(20),
+    zipcode Varchar(10)
+
+);
+"""
+
+cursor.execute(create_table_command)
+for row in item3:
+    cursor.execute("INSERT INTO userinfo VALUES (%s,%s,%s,%s,%s)",
+    (row[0],row[1],row[2],row[3],row[4]))
+
+
+
+with open('u.data', encoding = "latin1") as f:
+    reader = csv.reader(f, delimiter='\t')
+    item = []
+    for row in reader:
+        item.append(row)
+
+cursor.execute("DROP TABLE IF EXISTS main_data;")
+
+create_table_command = """
+CREATE TABLE main_data (
+    id int references userinfo(userid),
+    itemid int references movie_data(movie_id),
+    rating int,
+    timestmp int
 
 );
 """
 
 cursor.execute(create_table_command)
 for row in item:
-    cursor.execute("INSERT INTO genre VALUES (%s,%s)",
-    (row[1],row[0]))
+    cursor.execute("INSERT INTO main_data VALUES (%s,%s,%s,%s)",
+    (row[0],row[1],row[2],row[3]))
+
+
 
 
 connection.commit()
--- test.py
+++ test.py
@@ -1,20 +1,12 @@
 
-with open('u.genre', encoding = "latin1") as h:
-    reader3 = csv.reader(h, delimiter='|')
-    for row in reader3:
-        print(row)
 
-cursor.execute("DROP TABLE IF EXISTS genre;")
+import csv
 
-create_table_command = """
-CREATE TABLE genre (
-    id int,
-    name varchar(20)
 
-);
-"""
+with open('u.item', encoding = "latin1") as g:
+    reader2 = csv.reader(g, delimiter='|')
+    item2 = []
+    for row in reader2:
+        item2.append(row)
 
-cursor.execute(create_table_command)
-for row in item:
-    cursor.execute("INSERT INTO genre VALUES (%s,%s)",
-    (row[1],row[0]))
+print(item2)
